7e5a6ca154b17691d3fb917e16ba2cab86017205
Jackson Gray Morgan <jacksonmo@me.com>
2020-01-02 18:48:28 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,4 +5,4 @@ def meal_choice(protein = "meat", veg1, veg2)
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice("broccoli", "macaroni")
+meal_choice("tofu", "broccoli", "macaroni")
